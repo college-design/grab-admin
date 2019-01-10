85020c7c59aa2e7f5d68e773ab38bc100edae721
guoliang <guoliang@hrtpayment.com>
2019-01-10 15:24:37 +0800

    :o: Add springboot config

---
+++ db/db.sql
@@ -0,0 +1,10 @@
+-- auto-generated definition
+use lxg_test;
+DROP TABLE IF EXISTS user;
+create table user
+(
+  id   int auto_increment
+    primary key,
+  name varchar(50) null,
+  age  int         null
+);
